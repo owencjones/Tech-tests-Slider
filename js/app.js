a05a52633445ad86d7cacabab2a3104f3941271a
Owen C. Jones <owencjones@gmail.com>
2016-10-16 02:16:27 +0100

    First working version

--- js/app.js
+++ js/app.js
@@ -2,9 +2,34 @@
 
     $(document).ready(function () {
 
-        var config = {},
-            slider = new Slider(config);
+        var slides = [
+            new slide('Slide 1', 'Lorem Ipsum Dolor Consecteur Adipscing Elit', 'http://example.com/slide-1', 'img/001.png', 'Slide 1'),
+            new slide('Slide 2', 'Lorem Ipsum Dolor Consecteur Adipscing Elit', 'http://example.com/slide-2', 'img/002.png', 'Slide 2'),
+            new slide('Slide 3', 'Lorem Ipsum Dolor Consecteur Adipscing Elit', 'http://example.com/slide-3', 'img/003.png', 'Slide 3'),
+            new slide('Slide 4', 'Lorem Ipsum Dolor Consecteur Adipscing Elit', 'http://example.com/slide-4', 'img/004.png', 'Slide 4'),
+            new slide('Slide 5', 'Lorem Ipsum Dolor Consecteur Adipscing Elit', 'http://example.com/slide-5', 'img/005.png', 'Slide 5'),
+        ];
+
+        var config = {};
+
+        config.images = slides;
+
+        var slider = new Slider(config);
 
     });
+
+    function slide(title, blurb, link, img, alt) {
+
+        var _ret = {};
+
+        _ret.title = title;
+        _ret.blurb = blurb;
+        _ret.link = link;
+        _ret.img = img;
+        _ret.alt = alt;
+
+        return _ret;
+
+    }
 
 } (window.jQuery));
--- js/slider.js
+++ js/slider.js
@@ -13,7 +13,7 @@ var Slider;
 
         try {
 
-            _priv.configLoaded = $.extend({}, config, _priv.defaultConfig);
+            _priv.configLoaded = $.extend({}, _priv.defaultConfig, config);
 
         } catch (e) {
 
@@ -44,8 +44,19 @@ var Slider;
 
             var markUp = '';
 
-            markUp += '<div class="innerContainer">';
-            markUp += '<div class="loadingContainer">Loading...</div>';
+            markUp += '<div class="innerContainer loaded">';
+            markUp += '    <div class="loadingContainer">Loading...</div>';
+            markUp += '    <div class="main-slider">';
+            markUp += '        <div class="image-row">';
+            markUp += '        </div>';
+            markUp += '        <div class="bottom-bar">'
+            markUp += '            <h1 class="title"></h1>';
+            markUp += '            <p class="intro"><span class="intro-text"></span> <a href="#">Read more &raquo;</a></p>';
+            markUp += '            <div class="pager">';
+            markUp += '            </div>';
+            markUp += '        </div>';
+            markUp += '        <a href="#" class="full-space-link"></a>';
+            markUp += '    </div>';
             markUp += '</div>';
 
             var domElements = $(markUp);
@@ -60,8 +71,48 @@ var Slider;
 
     }
 
-    _methods.prevSlide = function () {};
-    _methods.nextSlide = function () {};
+    _priv.imageArray = null;
+    _priv.imageRow = null;
+    _priv.currentSlide = null;
+    _priv.currentTitle = null;
+    _priv.currentIntro = null;
+    _priv.currentLink = null;
+    _priv.globalLink = null;
+    _priv.pager = null;
+
+    _priv.eventLoop = null;
+
+    _priv.setupCurrentSlide = function () {
+
+        // Set text
+        var index = _priv.currentSlide || 0,
+            entry = _priv.imageArray[index];
+
+        _priv.currentTitle.text(entry.title);
+        _priv.currentIntro.text(entry.blurb);
+        _priv.currentLink.attr('href', entry.link);
+        _priv.globalLink.attr('href', entry.link);
+
+        // Set position
+        _priv.imageRow.css('margin-left', parseInt(index * -100) + '%');
+
+        _priv.pager.children('.slide-link').removeClass('active');
+        _priv.pager.find('.slide-link[data-link-index=' + _priv.currentSlide + ']').addClass('active');
+
+    }
+
+    _methods.prevSlide = function () {
+        _priv.currentSlide--;
+        if (_priv.currentSlide < 0) _priv.currentSlide = _priv.imageArray.length;
+
+        _priv.setupCurrentSlide();
+    };
+    _methods.nextSlide = function () {
+        _priv.currentSlide++;
+        if (_priv.currentSlide > _priv.imageArray.length - 1) _priv.currentSlide = 0;
+
+        _priv.setupCurrentSlide();
+    };
 
     Slider = function (config) {
 
@@ -74,7 +125,63 @@ var Slider;
         _priv.attachConfig(this.config.sliderContainer);
         _priv.innerDOM(this.config.sliderContainer);
 
-        return this
+        _priv.imageArray = this.config.images;
+
+        if ($.isArray(_priv.imageArray) && _priv.imageArray.length > 0) {
+
+            _priv.imageRow = $('.image-row');
+            _priv.pager = $('.pager');
+
+            // Load and append images
+            var imagesLoadedCount = 0,
+                imagesToLoad = _priv.imageArray.length,
+                imagesErrored = 0,
+                imagesRemaining = function () { return imagesToLoad - imagesLoadedCount - imagesErrored; };
+
+            $.each(_priv.imageArray, function (i, el) {
+
+                var imageToAdd = new Image();
+                imageToAdd.alt = el.alt;
+                imageToAdd.setAttribute('data-index', i);
+                imageToAdd.src = el.img;
+                imageToAdd.onload = function () {
+                    _priv.imageRow.append(imageToAdd);
+                    imagesLoadedCount++;
+
+                    _priv.pager.append('<span class="slide-link" data-link-index="' + $(this).data('index') + '"></span>');
+
+                };
+                imageToAdd.onerror = function () {
+                    imagesErrored++;
+                };
+
+            });
+
+            // Loop and wait until all loaded
+            var loadLoop = setInterval(function () {
+                if (imagesRemaining() === 0) {
+                    imagesLoaded();
+                    _priv.currentSlide = 0;
+                    clearInterval(loadLoop);
+                }
+            }, 20);
+
+            function imagesLoaded () {
+
+                _priv.currentTitle = $('.title');
+                _priv.currentIntro = $('.intro-text');
+                _priv.currentLink = $('.intro > a');
+                _priv.globalLink = $('.full-space-link');
+                _priv.imageRow.css('transition-duration', _priv.configLoaded.txTime + 'ms');
+                _priv.setupCurrentSlide();
+
+                _priv.eventLoop = setInterval(_methods.nextSlide, _priv.configLoaded.timeout);
+
+            }
+
+        }
+
+        return this;
 
     }
 
